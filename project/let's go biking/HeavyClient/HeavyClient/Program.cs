367e99fb419cf3ba1b80ea8d5f305f3bf55d284e
yassinumer <yassin-mohammed.umer@etu.unice.fr>
2021-04-27 23:33:47 +0200

    heavy client implementation

--- project/let's go biking/HeavyClient/HeavyClient/Program.cs
+++ project/let's go biking/HeavyClient/HeavyClient/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using HeavyClient.RoutingWithBikesSoap;
 
 namespace HeavyClient
@@ -12,7 +8,42 @@ namespace HeavyClient
         static void Main(string[] args)
         {
             ServiceClient service = new ServiceClient();
-            //service.getClosestStations();
+            Console.WriteLine("Please enter the departure address :");
+            string departure = Console.ReadLine();
+            Console.WriteLine("Please enter the destination address :");
+            string destination = Console.ReadLine();
+            Itinirary[] itiniraries = service.getClosestStations(departure, destination);
+            string[] stops = { departure, "pick up station", "drop off station", destination };
+            for(int i = 0; i < itiniraries.Length; i++)
+            {
+                Console.WriteLine("******");
+                Console.WriteLine("You are at a pin point");
+                Console.Write((i % 2 == 0) ? "walking" : "riding the bike");
+                Console.WriteLine(" from : " + stops[i] + "to destination\n");
+                foreach (Feature feature in itiniraries[i].features)
+                {
+                    foreach (Segment segment in feature.properties.segments)
+                    {
+                        foreach (Step step in segment.steps)
+                        {
+                            Console.WriteLine("\t" + step.instruction);
+                            Console.WriteLine("\tdistance : " + step.distance);
+                            Console.WriteLine("\tduration : " + step.duration);
+                            Console.WriteLine("");
+                            // simulate movement
+                            System.Threading.Thread.Sleep(700);
+                        }
+                    }
+                }
+                Console.WriteLine("******");
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("******");
+            Console.WriteLine("You have arrived at your destination : " + destination);
+            Console.WriteLine("******");
+            Console.WriteLine("\n");
+            Console.WriteLine("Press on any key to quit.");
+            Console.ReadLine();
         }
     }
 }
--- project/let's go biking/README.txt
+++ project/let's go biking/README.txt
@@ -11,5 +11,6 @@ Steps to launching the project :
    You just need to specify the fields (the departure and arrival addresses) and press on the "Find itinirary" button.
    For example in the departure input you can type in "Cours de la Gare, 94400 Vitry-sur-Seine" and "25 rue robert latouche" in the arrival.
 4. The heavy client is similar to the light one. You need to specify the same fields. The only difference is that it displays direction steps inside of the console instead of displaying the map with an itinirary.
+   You may find the have client's executable under : "HeavyClient\HeavyClient\bin\Debug\HeavyClient.exe".
 
 NB : The consoles of the self hosted applications display the URLs you must use the overall project.
